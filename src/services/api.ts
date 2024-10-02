b512396cef6808d98ef355d039853b66ec5cf16d
NilSousa16 <nil.sousa16@gmail.com>
2024-10-02 10:43:19 -0300

    Bug fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://18.223.171.40:8181/cxf/',
+// });
+
 const api = axios.create({
-  baseURL: 'http://18.223.171.40:8181/cxf/',
+  baseURL: '/api/',
 });
+
 
 export default api;
